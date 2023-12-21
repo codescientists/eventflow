97bb5584b164a5b033ee7687b84f6f2f6023a03c
codescientists <piyushpp2412@gmail.com>
2023-12-22 02:29:36 +0530

    Finish App

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -34,6 +34,7 @@ export const checkoutOrder = async (order: CheckoutOrderParams) => {
         buyerId: order.buyerId,
       },
       mode: 'payment',
+      billing_address_collection: 'required',
       success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/profile`,
       cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/`,
     });
